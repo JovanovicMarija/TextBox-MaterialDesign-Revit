bcb4719916ca63992c1ddcdf075a94f215d74baa
Marija Rakic <ga83cay@TUBVCMS-BIMPC01>
2019-05-09 10:38:37 +0200

    initial commit

---
+++ brisi/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using brisi;
+
+namespace Lab1PlaceGroup
+
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+
+        {
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            Window1 win = new Window1();
+            win.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ brisi/Window1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace brisi
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            ColorZoneAssist.SetMode(new CheckBox(), ColorZoneMode.Accent);
+            Hue hue = new Hue("xyz", System.Windows.Media.Color.FromArgb(1, 2, 3, 4), System.Windows.Media.Color.FromArgb(1, 5, 6, 7));
+            InitializeComponent();
+        }
+    }
+}
